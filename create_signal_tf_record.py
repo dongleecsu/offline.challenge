fd19222f958848c5c1be464b8833afd0a94408b4
DongLi <dongleecsu@gmail.com>
2017-10-28 20:48:55 +0800

    temp commit

--- create_signal_tf_record.py
+++ create_signal_tf_record.py
@@ -1,5 +1,7 @@
 import os
+import random
 import pickle
+import PIL.Image
 import tensorflow as tf
 
 flags = tf.app.flags
@@ -21,12 +23,48 @@ def read_all_samples(src_path):
 
 def get_label_map_dict(src_path):
     '''Generate label_map dict'''
-    all_items = pickle.load(src_path).read()
-    
+    with open(src_path, 'rb') as f:
+        all_items = pickle.load(f)
+    label_map_dict = {}
+    counter = 1
+    for name, subdict in all_items.iteritems():
+        # For subdict,
+        # key 'names' --> label names
+        # key 'values' --> bbox coordinates
+        for name in subdict['names']:
+            if name not in label_map_dict.keys():
+                label_map_dict[name] = counter
+                counter += 1
+    return label_map_dict
 
+def dict_to_tf_sample(sample,
+                      label_map_dict,
+                      annotation):
+    image = PIL.Image.open(sample)
+    width, height = image.size
 
-def create_tf_record(parameter_list):
-    pass
+    xmin = []
+    ymin = []
+    xmax = []
+    ymax = []
+    classes = []
+    classes_text = []
+
+    xmin.append(float(annotation['']))
+
+def create_tf_record(output_filename,
+                     samples_list,
+                     label_map_dict,
+                     annotations_path):
+    writer = tf.python_io.TFRecordWriter(output_filename)
+    for idx, sample in enumerate(samples_list):
+        for idx % 100 == 0:
+            print 'On image %d of %d', idx, len(samples_list)
+        tf_sample = dict_to_tf_sample(sample, 
+                                      label_map_dict, 
+                                      annotations_path)
+        writer.write(tf_sample.SerializeToString())
+    writer.close()
 
 def main(_):
     '''Pipeline
@@ -40,6 +78,21 @@ def main(_):
     samples_list = read_all_samples(data_dir)
 
     # Shuffle and generate train/val list
+    random.seed(1234)
+    random.shuffle(samples_list)
+    num_samples = len(samples_list)
+    num_train = int(0.7 * num_samples)
+    train_samples_list = samples_list[:num_train]
+    val_samples_list = samples_list[num_train:]
+    print '%d training and %d validation samples.',
+            len(train_samples_list), , len(val_samples_list)
+    
+    train_out_path = os.path.join(FLAGS.output_dir, 'signal_trian.record')
+    val_out_path = os.path.join(FLAGS.output_dir, 'signal_val.record')
+    create_tf_record(train_out_path, train_samples_list, 
+                    label_map_dict, annotations_path)
+    create_tf_record(val_out_path, val_samples_list, 
+                    label_map_dict, annotations_path)
 
 
     # Create tf record
